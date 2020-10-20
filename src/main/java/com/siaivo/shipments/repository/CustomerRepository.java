e8b421f4378630062719b3d1ba9d59f3968aa5d7
Maksym <maksym.murza@gmail.com>
2020-10-20 09:47:43 +0300

    model customer and customerRepository added

---
+++ src/main/java/com/siaivo/shipments/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.siaivo.shipments.repository;
+
+import com.siaivo.shipments.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("productRepository")
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+    Customer findByCustomerName(String customerName);
+    Customer findByCustomerCountry (String customerCountry);
+    Customer findById(int id);
+
+}
